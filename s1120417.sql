3ed192e2699fc0ece8ca736fa01b6b810d9292df
rudy870103 <favoriteinfinite@gmail.com>
2024-01-16 15:38:00 +0800

    匯出資料表

--- s1120417.sql
+++ s1120417.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2024-01-15 23:48:34
+-- 產生時間： 2024-01-16 08:36:29
 -- 伺服器版本： 10.4.28-MariaDB
 -- PHP 版本： 8.2.4
 
@@ -18,8 +18,31 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `animate`
+-- 資料庫： `s1120417`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `motto`
+--
+
+CREATE TABLE `motto` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `text` text NOT NULL,
+  `sh` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `motto`
+--
+
+INSERT INTO `motto` (`id`, `text`, `sh`) VALUES
+(1, '勇敢並不是什麼都不怕，而是即使恐懼，仍勇往直前', '0'),
+(2, '不要怕測試，與出錯混熟', '0'),
+(3, '看破不說破', '0'),
+(5, '更換座右銘成功', '0'),
+(6, '2024/01/18-作業deadline', '1');
 
 -- --------------------------------------------------------
 
@@ -40,11 +63,18 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `acc`, `pw`, `email`) VALUES
 (1, 'admin', '1234', 'favoriteinfinite@gmail.com'),
-(2, 'rudy', '0103', 'favoriteinfinite@gmail.com');
+(2, 'rudy', '0103', 'favoriteinfinite@gmail.com'),
+(4, 'rudolph', 'infinite0103', 'infinite0103');
 
 --
 -- 已傾印資料表的索引
 --
+
+--
+-- 資料表索引 `motto`
+--
+ALTER TABLE `motto`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- 資料表索引 `user`
@@ -57,10 +87,16 @@ ALTER TABLE `user`
 --
 
 --
+-- 使用資料表自動遞增(AUTO_INCREMENT) `motto`
+--
+ALTER TABLE `motto`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- 使用資料表自動遞增(AUTO_INCREMENT) `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
